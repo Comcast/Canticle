0772c9e8dd763a01bfcd03682ae4fcb54aeddda8
Richard Fliam <Richard_Fliam@cable.comcast.com>
2015-09-02 15:01:05 -0600

    Add in an update setting for get to update local branches against remotes

--- canticles/cantdeploader.go
+++ canticles/cantdeploader.go
@@ -13,6 +13,8 @@ type CanticleDepLoader struct {
 	Reader   CantDepReader
 	Resolver RepoResolver
 	Gopath   string
+	Update   bool
+	updated  map[string]string
 }
 
 func (cdl *CanticleDepLoader) FetchPath(path string) error {
@@ -33,16 +35,36 @@ func (cdl *CanticleDepLoader) FetchPath(path string) error {
 }
 
 func (cdl *CanticleDepLoader) FetchDeps(cdeps ...*CanticleDependency) error {
+	cdl.updated = make(map[string]string)
 	for _, cdep := range cdeps {
 		LogVerbose("Resolving repo for cdep %+v", cdep)
 		vcs, err := cdl.Resolver.ResolveRepo(cdep.Root, cdep)
 		if err != nil {
-			return fmt.Errorf("%v version control %s", cdep, err.Error())
+			return fmt.Errorf("cant create vcs for %v because %s", cdep, err.Error())
 		}
 		LogVerbose("Fetching cdep %+v", cdep)
 		if err := vcs.Create(cdep.Revision); err != nil {
-			return fmt.Errorf("failed to fetch because %s", err.Error())
+			return fmt.Errorf("cant fetch repo %s because %s", cdep.Root, err.Error())
 		}
+		if cdl.Update {
+			LogVerbose("Updating cdep %+v", cdep)
+			updated, res, err := vcs.UpdateBranch(cdep.Revision)
+			if err != nil {
+				return fmt.Errorf("cant update repo %s because %s", cdep.Root, err.Error())
+			}
+			if updated {
+				cdl.updated[cdep.Root] = res
+			} else {
+				LogVerbose("Did not update %s", res)
+			}
+		}
+		LogVerbose("\n")
 	}
 	return nil
+}
+
+// Updated returns a map of repo roots that where updated by the last
+// fetch deps/fetchpath call and the resulting info from the update.
+func (cdl *CanticleDepLoader) Updated() map[string]string {
+	return cdl.updated
 }
