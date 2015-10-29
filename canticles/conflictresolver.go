680fc2f3f5f4053ff41b3c19fac7185fd58c304f
Richard Fliam <Richard_Fliam@cable.comcast.com>
2015-10-29 15:58:50 -0600

    Let the prompt resolution conflict resolver deal with items with no revision or source

--- canticles/conflictresolver.go
+++ canticles/conflictresolver.go
@@ -26,9 +26,11 @@ func (pl *PreferLocalResolution) ResolveConflicts(deps *DependencySources) ([]*C
 }
 
 type PromptResolution struct {
+	Printf func(format string, a ...interface{}) (n int, err error)
+	Scanf  func(format string, a ...interface{}) (n int, err error)
 }
 
-func (pr *PromptResolution) ResolveConflicts(deps *DependencySources) ([]*CanticleDependency, error) {
+func (pr PromptResolution) ResolveConflicts(deps *DependencySources) ([]*CanticleDependency, error) {
 	cdeps := make([]*CanticleDependency, 0, len(deps.Sources))
 	for _, dep := range deps.Sources {
 		cd, err := pr.ResolveConflict(dep)
@@ -40,48 +42,52 @@ func (pr *PromptResolution) ResolveConflicts(deps *DependencySources) ([]*Cantic
 	return cdeps, nil
 }
 
-func (pr *PromptResolution) ResolveConflict(dep *DependencySource) (*CanticleDependency, error) {
+func (pr PromptResolution) ResolveConflict(dep *DependencySource) (*CanticleDependency, error) {
 	cd := &CanticleDependency{Root: dep.Root}
 	var err error
-	if dep.Revisions.Size() > 1 {
+	size := dep.Revisions.Size()
+	switch {
+	case size > 1:
 		cd.Revision, err = pr.SelectRevision(dep)
 		if err != nil {
 			return cd, err
 		}
-	} else {
+	case size == 1:
 		cd.Revision = dep.Revisions.Array()[0]
 	}
-	if dep.Sources.Size() > 1 {
+	size = dep.Sources.Size()
+	switch {
+	case size > 1:
 		cd.SourcePath, err = pr.SelectSource(dep)
-	} else {
+	case size == 1:
 		cd.SourcePath = dep.Sources.Array()[0]
 	}
 	return cd, err
 }
 
-func (pr *PromptResolution) SelectRevision(dep *DependencySource) (string, error) {
-	return ResolvePrompt(dep.Root, "revisions", dep.OnDiskRevision, dep.Revisions.Array())
+func (pr PromptResolution) SelectRevision(dep *DependencySource) (string, error) {
+	return pr.ResolvePrompt(dep.Root, "revisions", dep.OnDiskRevision, dep.Revisions.Array())
 }
 
-func (pr *PromptResolution) SelectSource(dep *DependencySource) (string, error) {
-	return ResolvePrompt(dep.Root, "sources", dep.OnDiskSource, dep.Sources.Array())
+func (pr PromptResolution) SelectSource(dep *DependencySource) (string, error) {
+	return pr.ResolvePrompt(dep.Root, "sources", dep.OnDiskSource, dep.Sources.Array())
 }
 
 // ResolvePrompt is used to prompt a user for a resolution between
 // multiple alternates, wth ondisk marking the currently select
 // option.
 // TODO: Add some sort of auto completion here
-func ResolvePrompt(pkg, conflict, ondisk string, alts []string) (string, error) {
-	fmt.Printf("\nPackage %s has conflicting %s:\n", pkg, conflict)
+func (pr PromptResolution) ResolvePrompt(pkg, conflict, ondisk string, alts []string) (string, error) {
+	pr.Printf("\nPackage %s has conflicting %s:\n", pkg, conflict)
 	for _, rev := range alts {
 		if rev == ondisk {
-			fmt.Println(rev, "(current)")
+			pr.Printf("%s %s", rev, "(current)")
 			continue
 		}
-		fmt.Println(rev)
+		pr.Printf("%s\n", rev)
 	}
-	fmt.Printf("Selection %s: ", conflict)
+	pr.Printf("Selection %s: ", conflict)
 	var choice string
-	_, err := fmt.Scanf("%s", &choice)
+	_, err := pr.Scanf("%s", &choice)
 	return choice, err
 }
