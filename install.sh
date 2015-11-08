c0fbfe44e536ceb6b3921d0a7a243f064b9d7290
rfliam <Richard_Fliam@cable.comcast.com>
2015-11-07 18:54:55 -0700

    Make this more go get friendly, and update the install script to work with github.com. Update website docs to use the install script.

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
 cantdir="$GOPATH/src/github.com/Comcast/Canticle"
-vcstools="golang.org/x/tools/go/vcs"
-go get $vcstools
-mkdir -p $cantdir
-git clone https://github.com/Comcast/Canticle $cantdir
+go get "github.com/Comcast/Canticle/..."
 pushd $cantdir
-go build cant.go
-./cant genversion
-go build
-mkdir -p "$GOPATH/bin/"
-cp cant "$GOPATH/bin/cant"
+cant genversion
+go install "github.com/Comcast/Canticle/cant"
 popd
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf $GOPATH/bin/cant
-rm -rf $GOPATH/build/github.comcast.com/viper-cog/cant
-rm -rf $GOPATH/src/github.comcast.com/viper-cog/cant
+rm -rf $GOPATH/build/github.com/Comcast/Canticle
+rm -rf $GOPATH/src/github.com/Comcast/Canticle
